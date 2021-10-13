240822230509fa0eb2c3a24b1bc5ec14989bf0e4
Asad Zaheer <asadzaheer1408@gmail.com>
2021-10-13 21:46:27 +0300

    update: search result suggestion dropdown

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -16,20 +16,45 @@ class SearchController extends AbstractController
         $query = $request->query->get('search');
 
         if (!empty($query)) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $repo = $entityManager->getRepository("App:Post");
-
-            $results = $repo->createQueryBuilder('post')
-                ->where('post.title LIKE :search')
-                ->orWhere('post.content LIKE :search')
-                ->setParameter(':search', "%${query}%")
-                ->getQuery()
-                ->getResult();
+            $results = $this->getSearchedPosts($query);
         }
 
         return $this->render('search/index.html.twig', [
             'results' => $results,
             'query' => $query,
         ]);
+    }
+
+    #[Route('/ajax-search', name: 'ajax_search')]
+    public function ajaxSearch(Request $request): Response
+    {
+        $results = null;
+        $query = $request->query->get('search');
+
+        if (!empty($query)) {
+            $posts = $this->getSearchedPosts($query);
+        }
+
+        foreach ($posts as $post){
+            $results[] = [
+                "id" => $post->getId(),
+                "title" => $post->getTitle(),
+            ];
+        }
+
+        return new Response(json_encode($results));
+    }
+
+    private function getSearchedPosts (string $query): array
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $repo = $entityManager->getRepository("App:Post");
+        
+        return $repo->createQueryBuilder('post')
+            ->where('post.title LIKE :search')
+            ->orWhere('post.content LIKE :search')
+            ->setParameter(':search', "%${query}%")
+            ->getQuery()
+            ->getResult();
     }
 }
